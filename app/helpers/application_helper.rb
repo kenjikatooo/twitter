0df6f1c41668f85ff3d81435dfc42770d2bb0cc0
kenjikatooo <k.524.10nis@gmail.com>
2018-04-01 18:46:18 +0900

    full_titleメソッドを作成しページごとに完全なタイトルを返すようにした

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+    #ページごとの完全なタイトルを返す
+    #full_titleメソッドを作成
+    def full_title(page_title='')
+        base_title = "twitter"
+        if page_title.empty?
+            base_title
+        else
+            page_title+ " | "+ base_title
+        end
+    end
 end
