be5d04ea9b8ed06bbc1c6bb0a1a2c015c752898d
kenjikatooo <k.524.10nis@gmail.com>
2018-04-04 02:22:06 +0900

    新規登録画面のフォームの完成と失敗時のエラーの表示、成功時のプロフィール画面への遷移を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,25 @@ class UsersController < ApplicationController
     #@userを定義しshow.htmlに渡す
     @user = User.find(params[:id])
   end
+
+  def create
+    #User.new(name~~)とやってることは一緒でそれをparamsでやってる
+    @user = User.new(user_params) #まだ実装は終わってないぜ
+    if @user.save
+      flash[:success] = "ようこそtwitterへ"
+      #ユーザーのページへ返す（ユーザー＝@user）
+      redirect_to @user
+      #redirect_to はuser_url(@user)と一緒
+    else
+      #routesの設定と同じでnewアクションが実行される
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    #name, emailなどってくる情報をここで指定することでセキュリティを高める
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
