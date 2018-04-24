c221a2139af4d87f4504bdef4f55c11dbd99bc8a
kenjikatooo <k.524.10nis@gmail.com>
2018-04-24 13:32:18 +0900

    アクションメーラーの生成とコントローラーの作成を行った

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token
+    attr_accessor :remember_token, :activation_token
     #保存前に強制的にメールアドレスを小文字に変換
-    before_save { self.email = email.downcase }
+    before_save :downcase_email
+    before_create :create_activation_digest
     #validateメソッドを使って情報の有効性を定める
     validates :name,  presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -38,4 +39,14 @@ class User < ApplicationRecord
         #ユーザーのログイン情報を破棄する
         update_attribute(:remember_digest, nil)
     end
+
+    private
+        def downcase_email
+            self.email = email.downcase
+        end
+
+        def create_activation_digest
+            self.activation_token = User.new_token
+            self.activation_digest = User.digest(activation_token)
+        end
 end
